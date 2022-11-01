37fe35aaf61f3290c3c2e1c3a8e1356c7494cd69
Bogdan <74682561+inspiq@users.noreply.github.com>
2022-11-02 00:00:38 +0400

    code improvement

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -19,15 +19,11 @@ export const useStore = defineStore("store", {
     cart: cart ? JSON.parse(cart) : [],
     isLoading: true,
     currentProduct: null,
-<<<<<<< HEAD
-=======
-    totalPrice: totalPrice ? parseInt(totalPrice) : 0
->>>>>>> 4ccf926a9cf9d24c46528f31cf7583b1b1e9e709
   } as State),
   getters: {
     getTotalPrice(state) {
-      return state.cart.reduce((a, b) => {
-        return Math.round(a += b.price * b.rating.count)
+      return state.cart.reduce((totalPrice, product) => {
+        return Math.round(totalPrice += product.price * product.rating.count)
       }, 0)
     }
   },
